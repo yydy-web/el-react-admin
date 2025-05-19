8719fbb0ef0a4e0b8c040bc6feeebb2c1661f520
a949066041 <949066041@qq.com>
2025-05-19 14:56:19 +0800

    chore: update dep

--- src/routes/_auth/store.lazy.tsx
+++ src/routes/_auth/store.lazy.tsx
@@ -17,10 +17,10 @@ function RouteComponent() {
         {count}
       </span>
       <div className=" space-x-3">
-        <button onClick={() => increment(1)} type="button" className=" rounded-lg border-orange-500 border-solid border px-20">
+        <button onClick={() => increment(1)} type="button" className=" rounded-lg border-orange-500 border-solid border px-20 cursor-pointer">
           +1
         </button>
-        <button onClick={() => decrement(1)} type="button" className=" rounded-lg border-orange-500 border-solid border px-20">
+        <button onClick={() => decrement(1)} type="button" className=" rounded-lg border-orange-500 border-solid border px-20 cursor-pointer">
           -1
         </button>
       </div>
